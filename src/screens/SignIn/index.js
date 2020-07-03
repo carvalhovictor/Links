2722ba54b0235dfe5e030fd2d7b93a9f007676be
Victor Carvalho <victorcamargocarvalho@gmail.com>
2020-07-03 14:49:44 -0300

    Creating links

--- src/screens/SignIn/index.js
+++ src/screens/SignIn/index.js
@@ -1,16 +1,15 @@
 import React from "react";
 import { Link, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
-import {signIn} from "../../actions/AccountActions";
+import { signIn } from "../../actions/AccountActions";
+import { getFormData } from "../../helpers/form";
 
 const SignIn = props => {
 	const { account, signIn } = props;
 
 	const submitHandler = e => {
 		e.preventDefault();
-		
-		const formData = new FormData(e.target);
-		const data = Object.fromEntries(formData);
+		const data = getFormData(e);
 		signIn(data);
 	}
 
