2a06d86a837b65c1c86a0abf34a5c53553e19504
Victor Carvalho <victorcamargocarvalho@gmail.com>
2020-07-03 17:29:37 -0300

    Delete implemented

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -18,18 +18,29 @@ export const apiPost = (path, data = {}) => {
 const url = getApiUrl(path);
 const options = {
 	headers: getHeaders()
-};
+	};
 
 return axios.post(url, data, options);
 };
+
 export const apiPut= (path, data = {}) => {
 	const url = getApiUrl(path);
 	const options = {
 		headers: getHeaders()
 	};
 	
-	return axios.put(url, data, options);
+	return axios.put(url, options);
+};
+
+export const apiDelete= (path, data = {}) => {
+	const url = getApiUrl(path);
+	const options = {
+		headers: getHeaders()
 	};
+	
+	return axios.delete(url, options);
+};
+
 
 export const apiGet = (path) => {
 	const url = getApiUrl(path);
--- src/screens/Manage/Links/index.js
+++ src/screens/Manage/Links/index.js
@@ -4,16 +4,18 @@ import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
 import Layout from "../../Layouts/Manage";
-import { linkList, setLinkToRemove } from "../../../actions/LinkActions";
+import { linkList, setLinkToRemove, linkRemove } from "../../../actions/LinkActions";
 
 
-const Links = ({ links, linkList, linkToRemove, setLinkToRemove }) => {
+const Links = ({ links, linkRemove, linkList, linkToRemove, setLinkToRemove }) => {
 
 	useEffect(() => {
 		linkList();
 	}, [linkList]);
 
-	console.log("*** Links.linkToRemove", linkToRemove);
+	const cancelDelete = e => setLinkToRemove(null);
+	const confirmDelete = e => (linkToRemove ? linkRemove(linkToRemove) : null);
+
 	return (
 		<Layout>
 			<div className="row">
@@ -51,6 +53,18 @@ const Links = ({ links, linkList, linkToRemove, setLinkToRemove }) => {
 				</div>
 				);
 				}) :null}
+
+				{linkToRemove ? (
+				<div className="alert alert-danger rounded float-center shadow-bold">
+					<h4 className="alert-heading">Delete Confirmation!</h4>
+					<p>Are you sure you want to delete, this action cannot be undone.</p>
+					<hr />
+					<div className="d-flex justify-content-between">
+						<button className="btn btn-outline-light" onClick={cancelDelete}>Cancel</button>
+						<button className="btn btn-danger" onClick={confirmDelete}>Delete</button>
+					</div>
+				</div>
+				) : null}
 		</Layout> 
 	);
 };
@@ -62,4 +76,4 @@ const mapStateToProps = state => {
 	};
 }
 
-export default connect(mapStateToProps, {linkList, setLinkToRemove })(Links);
+export default connect(mapStateToProps, {linkList, setLinkToRemove, linkRemove })(Links);
